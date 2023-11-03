883fa4c39205d57b7413583de6cf8f579a10f010
Naufal Nasrullah <naufaln176@gmail.com>
2023-11-03 14:23:52 +0700

    fixing navigate bug

--- src/router/private.tsx
+++ src/router/private.tsx
@@ -1,13 +1,14 @@
 import { selectGetSession, selectIsAuth, useAuthStore } from "@/store/auth";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export default function PrivateRoute() {
   const getSession = useAuthStore(selectGetSession);
   const auth = useAuthStore(selectIsAuth);
+  const navigate = useNavigate();
   useEffect(() => {
     getSession();
   }, [getSession]);
-  if (auth === true) return <Outlet />;
-  return <Navigate to="/" />;
+  if (auth) return <Outlet />;
+  navigate("/")
 }
