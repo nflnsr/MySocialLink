24954925ee9323a11be8324845070fc94f35b605
Naufal Nasrullah <naufaln176@gmail.com>
2023-12-20 07:01:57 +0700

    add datadog rum integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import Index from "@/router"
 import { useEffect } from "react";
 import { useTheme } from "@/store/theme";
 import { applyThemePreference } from "@/utils/theme";
+import { initDatadogRum } from '@/lib/datadog-rum';
 
 export default function App() {
   const theme = useTheme();
+  
   useEffect(() => {
     applyThemePreference(theme);
   }, [theme]);
+
+  useEffect(() => {
+    initDatadogRum();
+  }, []);
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
